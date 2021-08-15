f1e1f5d9efb10793e919c556c883ded7dc552094
Joshua Dinh <joshuahungdinh@gmail.com>
2021-08-15 08:59:43 -0700

    updated with router

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import Row from "./components/Row/Row";
 import requests from "./requests";
 import Banner from "./components/Banner/Banner";
 import Navigation from "./components/Navigation/Navigation";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Banner />
-      <Row
-        isLargeRow
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-      />
-      <Row title="Trending" fetchUrl={requests.fetchTrending} />
-      <Row title="Action" fetchUrl={requests.fetchAction} />
-      <Row title="Comedy" fetchUrl={requests.fetchComedy} />
-      <Row title="Horror" fetchUrl={requests.fetchHorror} />
-      <Row title="Romance" fetchUrl={requests.fetchRomance} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div className="App">
+            <Navigation />
+            <Banner />
+            <Row
+              isLargeRow
+              title="NETFLIX ORIGINALS"
+              fetchUrl={requests.fetchNetflixOriginals}
+            />
+            <Row title="Trending" fetchUrl={requests.fetchTrending} />
+            <Row title="Action" fetchUrl={requests.fetchAction} />
+            <Row title="Comedy" fetchUrl={requests.fetchComedy} />
+            <Row title="Horror" fetchUrl={requests.fetchHorror} />
+            <Row title="Romance" fetchUrl={requests.fetchRomance} />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
